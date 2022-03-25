f8d74aaa27342980fa9560b758a4e4c8e6325f7c
Cherrypick14 <cherrypicklet@gmail.com>
2022-03-26 01:10:29 +0300

    setting up sec key

--- learningbuddie/settings.py
+++ learningbuddie/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fhnf7n)-^(fsi$2&e3@$9le=j9t8!$b)-k%=!uf27hcj#rnhd9'
+SECRET_KEY = os.getenv('SECRET KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
